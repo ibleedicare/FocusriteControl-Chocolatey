b48024cb56a00a0b5420bc9f75f9fddcd1d9d304
IBleedIcare <ibleedicare@gmail.com>
2023-11-06 15:30:41 +0100

    Initial Commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://fael-downloads-prod.focusrite.com/customer/prod/downloads/focusrite_control_3.15.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'FocusriteControl*'
+
+  checksum      = 'ACDA29699C04FC6B509906C3B05E4C36CB4DB8BE9A4A45FB30106044058E068C'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
